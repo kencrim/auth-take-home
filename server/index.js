c38970786d02313162b1c30a6a7cf505fcb18b74
Ken Crimmins <kencrimmins@Kens-MacBook-Pro.local>
2019-03-07 21:23:11 -0800

    Added basic server infrastructure for tokens

--- server/index.js
+++ server/index.js
@@ -1,13 +1,41 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const jwt = require('jsonwebtoken');
 
-app.get('/auth',(req, res) => {
+const PORT = 3000;
+const app = express();
+
+app.post('/api/users', verifyToken, (req, res) => {
 	res.json({
-		message: 'Hello world!'
+		message: 'post created'
+	});
+});
+
+app.post('/api/login',(req, res) => {
+	const user = {
+		id: 1,
+		username: 'Ken',
+		email: 'kencrim@gmail.com'
+	}
+	jwt.sign({user},'key', (err, token) => {
+		res.json({
+			token
+		})
 	});
 });
 
 app.listen(PORT, () => {
 	console.log('Now listening on port ' + PORT);
 });
+
+// Verify Token
+
+function verifyToken(req, res, next) {
+	const bearerHeader = req.headers['authorization'];
+	if(typeof bearerHeader !== "undefined") {
+		res.json({
+			message: "Token Verified"
+		})
+	} else {
+		res.sendStatus(403);
+	}
+}
