87445adeef24a5e170cd605c190dd7ec6e603463
Ken Crimmins <kencrimmins@Kens-MacBook-Pro.local>
2019-03-09 14:03:04 -0800

    install mysql and added basic interface

--- database/index.js
+++ database/index.js
@@ -1,1 +1,17 @@
-index.js
+const mysql = require('mysql');
+const options = require('./db_config.js');
+
+const connection = mysql.createConnection(options);
+ 
+connection.connect();
+
+const findUser = (email, callback) => {
+	connection.query(`SELECT * FROM \`users\` WHERE \`email\` = "${email}"`, (err, results) => {
+		if(err) {
+			callback(err, null);
+		} else {
+			callback(null, results);
+		}
+	})
+}
+
