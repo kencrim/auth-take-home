90e51bd10b1b4242a04dde98e586f6c0b7e13f1c
Ken Crimmins <kencrimmins@Kens-MacBook-Pro.local>
2019-03-09 13:53:28 -0800

    add basic sql schema and comments to client

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,18 @@
-schema.sql
+DROP DATABASE IF EXISTS whitelist;
+CREATE DATABASE whitelist;
+
+USE whitelist;
+
+CREATE TABLE IF NOT EXISTS users (
+  `user_id` INTEGER NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(30 ) NOT NULL,
+  `first_name` VARCHAR(250) NULL DEFAULT NULL,
+  `last_id` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+);
+
+/*  Execute this file from the command line by running:
+ *    mysql -u root < database/schema.sql
+ *  from the root directory to create the database 
+ *  and the tables. This command will also be run as part of the 
+ *	npm start script */
